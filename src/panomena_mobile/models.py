04736c632b1d41a04b274f97e3bdcc9d718afec6
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-07-06 16:36:26 +0200

    Specified field type for msisdn field

--- src/panomena_mobile/models.py
+++ src/panomena_mobile/models.py
@@ -3,8 +3,12 @@ from django.db import models
 from panomena_mobile import fields
 
 
-class MsisdnField(models.Field):
+class MsisdnField(models.CharField):
     """Field for storing MSISDN values."""
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 32
+        super(MsisdnField, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
         """Returns the appropriate form field for working with

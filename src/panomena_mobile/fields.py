44bc9593057dffd3efeb15bee4f4274da671f1b5
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-07-22 10:38:50 +0200

    Implemented valid prefix for mobile number field

--- src/panomena_mobile/fields.py
+++ src/panomena_mobile/fields.py
@@ -14,17 +14,30 @@ class MsisdnField(forms.Field):
     default_error_messages = { 
         'invalid': _(u'Enter a valid mobile number.'),
         'invalid_country_code': _(u'Enter a number with the %s country code.'),
-        'too_short': _(u'Enter a mobile number with more digits.'),
-        'too_long': _(u'Enter a mobile number with less digits.'),
+        'invalid_prefix': _(u'Enter a number with a valid prefix.'),
+        'too_short': _(u'Enter a number with more digits.'),
+        'too_long': _(u'Enter a number with less digits.'),
     }
 
     def __init__(self, default_country_code=None, restrict_country_code=None,
-                 max_length=11, min_length=11, *args, **kwargs):
+                 valid_prefixes=None, max_length=11, min_length=11,
+                 *args, **kwargs):
         super(MsisdnField, self).__init__(*args, **kwargs)
         self.restrict_country_code = restrict_country_code
         self.default_country_code = default_country_code
+        self.valid_prefixes = valid_prefixes
         self.max_length = max_length
         self.min_length = min_length
+
+    def valid_prefix(self, number):
+        """Confirms a valid prefix."""
+        valid_prefixes = self.valid_prefixes
+        if self.valid_prefixes:
+            for prefix in valid_prefixes:
+                if number.startswith(prefix):
+                    return True
+            return False
+        return True
 
     def to_python(self, value):
         error_messages = self.error_messages
@@ -59,6 +72,9 @@ class MsisdnField(forms.Field):
             raise ValidationError(error_messages['too_long'])
         if len(msisdn) < self.min_length:
             raise ValidationError(error_messages['too_short'])
+        # check that the prefix is valid
+        if not self.valid_prefix(number):
+            raise ValidationError(error_messages['invalid_prefix'])
         # return the msisdn
         return country_code + number
     
